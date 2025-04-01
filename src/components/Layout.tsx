d2f947c7699e487b3de45ffc0786c2293a9c875e
Did85 <did85@hotmail.com>
2025-04-01 17:32:52 +0200

    fix: restore header and add version to auth page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { VersionInfo } from './VersionInfo';
+import { Header } from './Header';
 
 export function Layout() {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Votre contenu de layout existant */}
-      <Outlet />
+      <Header />
+      <main className="container mx-auto px-4 py-8">
+        <Outlet />
+      </main>
       <VersionInfo />
     </div>
   );
 }
+
 
